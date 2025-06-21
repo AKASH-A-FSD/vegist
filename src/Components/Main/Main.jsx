48ffe87368e15164df2c4af88ceaea3f8c3be314
AKASH-A-FSD <akashpersonal.a@gmail.com>
2025-06-21 16:00:05 +0530

    new

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import "./Main.css"
 const Main = () => {
   return (
     <>
-    <img src="https://vegina-store.myshopify.com/cdn/shop/files/slider19_899214a4-2a83-44b1-8071-16f298352363.jpg?v=1614371209" alt="" />
+    <div className="bg">
+     <div className="bgimg"> <img src="https://vegina-store.myshopify.com/cdn/shop/files/slider19_899214a4-2a83-44b1-8071-16f298352363.jpg?v=1614371209" alt="" /></div>
+      <div className="quote">
+        <p>Count memories, not calories</p>
+      </div>
+    </div>
     </>
   )
 }
